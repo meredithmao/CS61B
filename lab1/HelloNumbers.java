a5b65fafb8a24925d7dc3db97bfbe8f79e24c215
Meredith Mao <meredithmao@airbears2-10-142-35-84.airbears2.1918.berkeley.edu>
2018-01-19 12:47:36 -0800

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,11 @@
+public class HelloNumbers {
+    public static void main(String[] args) {
+        int x = 0;
+        int val = 0;
+        while (x < 10) {
+        	val = val + x;
+            System.out.print(val + " ");
+            x = x + 1;
+        }
+    }
+}

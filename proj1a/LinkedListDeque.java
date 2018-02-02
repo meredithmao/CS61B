b0f7d67591db74a320f1cf2ca7372bcd184addca
Meredith Mao <meredithmao@Merediths-MacBook-Pro.local>
2018-02-02 15:40:55 -0800

    adding files

--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,29 +1,25 @@
-public class LinkedListDeque <T> {
-
-	public class Node {
-		public Node prev;
-		public T item;
-		public Node next;
-
-		public Node(Node p, T i, Node n) {
-			this.prev = p;
-			this.item = i;
-			this.next = n;
-
-		}
-	}
+public class LinkedListDeque<T> {
+    public class Node {
+        private Node prev;
+        private T item;
+        private Node next;
+        public Node(Node p, T i, Node n) {
+        	this.prev = p;
+        	this.item = i;
+        	this.next = n;
+        }
+    }
 	/* The first item if it exists is at sentinel.next*/
 	private Node sentinel;
 	private int size;
 
-	public LinkedListDeque () {
+	public LinkedListDeque() {
 		sentinel = new Node(this.sentinel, null, this.sentinel);
 		size = 0;
 	}
-
-	public void addFirst (T item) {
+	public void addFirst(T item) {
 		//p.next = this.sentinel;
-		Node p = new Node (this.sentinel, item, this.sentinel.next);
+		Node p = new Node(this.sentinel, item, this.sentinel.next);
 		if (this.isEmpty()) {
 			this.sentinel.prev = p;
 			p.next = this.sentinel;
@@ -31,48 +27,36 @@ public class LinkedListDeque <T> {
 		this.sentinel.next = p;
 		this.size = this.size + 1;
 	}
-
 	public void addLast(T item) {
 		this.size = this.size + 1;
 	/*keep moving through the nodes */
 		Node oldNode = this.sentinel.prev;
-
-		Node newNode = new Node (oldNode, item, this.sentinel);
+		Node newNode = new Node(oldNode, item, this.sentinel);
 		oldNode.next = newNode;
 		this.sentinel.prev = newNode;
-
 	}
-
 	public boolean isEmpty() {
-		if (this.size == 0){
+		if (this.size == 0) {
 			return true;
 		}
-		else {
-			return false;
-		}
+		return false;
 	}
-
-	public int size () {
+	public int size() {
 		return this.size;
 	}
-
-	public void printDeque () {
+	public void printDeque() {
 		Node p = this.sentinel.next;
-
 		int count = 0;
 		while (count < this.size) {
 			System.out.print(p.item.toString() + " ");
 			p = p.next;
 			count += 1;
 		}
-
 	}
-
 	public T removeFirst() {
-		if(this.isEmpty()){
+		if (this.isEmpty()) {
 			return null;
-		}
-		else {
+		} else {
 			this.size = this.size - 1;
 			/* to figure out the first item */
 			Node firstitem = this.sentinel.next;
@@ -80,23 +64,19 @@ public class LinkedListDeque <T> {
 			/* move the second node to become the first node */
 			this.sentinel.next = firstitem.next;
 			firstitem.next.prev = this.sentinel;
-
 			return itemvalue;
 		}
 	}
-
 	public T removeLast() {
-		if (this.isEmpty()){
+		if (this.isEmpty()) {
 			return null;
-		}
-		else {
+		} else {
 			this.size = this.size - 1;
 			Node oldNode = this.sentinel.prev;
 			T itemvalue = oldNode.item;
-			/* remove the last item of the deque by repointing where the previous sentinel directs to */
+			/* remove the last item of the deque by repointing */
 			this.sentinel.prev = oldNode.prev;
 			oldNode.prev.next = this.sentinel;
-
 			return itemvalue;
 
 		}
@@ -108,11 +88,9 @@ public class LinkedListDeque <T> {
 
 		if (index > this.size) {
 			return null;
-		}
-		else if (this.isEmpty()) {
+		} else if (this.isEmpty()) {
 			return null;
-		}
-		else {
+		} else {
 			while (index > 0) {
 				p.next = p.next.next;
 				index = index - 1;
@@ -121,23 +99,20 @@ public class LinkedListDeque <T> {
 			return p.item;
 		}
 	}
-	private T RecursiveHelper (int index, Node p) {
+	private T recursiveHelper(int index, Node p) {
 		if (index == 0) {
 			return p.item;
-		}
-		else {
-			return RecursiveHelper(index - 1, p.next);
+		} else {
+			return recursiveHelper(index - 1, p.next);
 		}
 	}
-	public T getRecursive (int index) {
+	public T getRecursive(int index) {
 		if (index > this.size) {
 			return null;
-		}
-		else if (this.isEmpty()) {
+		} else if (this.isEmpty()) {
 			return null;
-		}
-		else {
-			return RecursiveHelper(index, this.sentinel.next);
+		} else {
+			return recursiveHelper(index, this.sentinel.next);
 		}
 	}
 }
